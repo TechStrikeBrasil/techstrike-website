7c6324086ff6c6802fa470d314b8b69d89f41938
Michel Alves <michelalvs@gmail.com>
2025-11-17 16:22:31 -0300

    Added englsh description on website

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ function App() {
       <div className="App">
         <h1>Em Construção</h1>
         <p>Em breve o novo site do nosso estúdio de jogos brazuca!</p>
-
+        
+        <p>Soon a new website for our brazilian games studio!</p>
       </div>
     </>
   )
